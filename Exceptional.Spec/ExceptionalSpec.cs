6ffca03ca0200b51828660c06bf02cfdebdedca0
bradphelan <b.phelan@weingartner.com>
2016-06-15 09:45:01 +0200

    add more specs

--- Exceptional.Spec/ExceptionalSpec.cs
+++ Exceptional.Spec/ExceptionalSpec.cs
@@ -89,5 +89,58 @@ namespace Weingartner.Exceptional.Spec
 
 
         }
+
+        [Fact]
+        public void SelectManyIsExceptionSafe()
+        {
+            var xa = Exceptional.Success(1);
+            var xb = Exceptional.Success(2);
+
+            Func<double, double, double> fn = (a, b) =>
+            {
+                throw new Exception("foo");
+            };
+
+            var xx =
+                from a in xa
+                from b in xb
+                select fn(a, b);
+
+
+            xx.HasException.Should().BeTrue();
+            xx.Exception.Message.Should().Be("foo");
+            new Action
+                (() =>
+                {
+                    var foo = xx.Value;
+                }).ShouldThrow<Exception>();
+            
+        }
+
+        [Fact]
+        public void SelectIsExceptionSafe()
+        {
+            var xa = Exceptional.Success(1);
+            var xb = Exceptional.Success(2);
+
+            Func<double, double, double> fn = (a, b) =>
+            {
+                throw new Exception("foo");
+            };
+
+            var xx =
+                from a in xa
+                select fn(a, 2);
+
+
+            xx.HasException.Should().BeTrue();
+            xx.Exception.Message.Should().Be("foo");
+            new Action
+                (() =>
+                {
+                    var foo = xx.Value;
+                }).ShouldThrow<Exception>();
+            
+        }
     }
 }
