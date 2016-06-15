51512279086ee4d1f2bcb7ac070c48e6f9d1b605
bradphelan <b.phelan@weingartner.com>
2016-06-15 09:29:52 +0200

    Update the title.

--- Exceptional/Properties/AssemblyInfo.cs
+++ Exceptional/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Exceptional")]
+[assembly: AssemblyTitle("Exceptional LINQ")]
 [assembly: AssemblyDescription("A LINQ compatible exception object")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Weingartner")]
